ed82069ae8b6830179f0883a97d4afed84abb11c
Megichu64 <amonmegan@gmail.com>
2025-11-22 18:48:24 -0800

    Updated html, css, and js to add Scroll Up button

--- script.js
+++ script.js
@@ -42,3 +42,23 @@ function playAudio(phrase) {
     console.log("User clicked play for:", phrase);
     alert("Playing audio for: " + phrase + " (Connect actual MP3 files in the code!)");
 }
+
+// --- 4. SCROLL TO TOP LOGIC ---
+// Get the button
+const mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", function() {
+    window.scrollTo({top: 0, behavior: 'smooth'}); // 'smooth' makes it glide nicely
+});
