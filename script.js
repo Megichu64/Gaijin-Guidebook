0f7d5991a6c8cb259ff5226c610696156fa15b72
Megichu64 <amonmegan@gmail.com>
2025-12-09 22:09:08 -0800

    Added lightbox model for pop-out photos on website

--- script.js
+++ script.js
@@ -286,3 +286,33 @@ if (track) {
         });
     });
 }
+// --- 10. LIGHTBOX LOGIC FOR BLOG IMAGES ---
+// LIGHTBOX FUNCTIONALITY
+
+// Get the lightbox elements
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close-lightbox');
+
+// Select all images you want to be clickable
+// (Ensure you added the 'clickable-img' class to your HTML images!)
+const images = document.querySelectorAll('.clickable-img');
+
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        lightbox.style.display = "block";
+        lightboxImg.src = image.src;
+    });
+});
+
+// Close the lightbox when clicking the (x)
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = "none";
+});
+
+// Close the lightbox when clicking outside the image
+lightbox.addEventListener('click', (e) => {
+    if (e.target !== lightboxImg) {
+        lightbox.style.display = "none";
+    }
+});
